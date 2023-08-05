64aeefdd8a199af5318f8197a9785fa673d38f0d
Sagi Forbes <sagi.forbes@myhome.loc>
2023-08-05 14:29:02 +0300

    dev

--- ubuntu-2204-k8s-install-control-plane-1.sh
+++ ubuntu-2204-k8s-install-control-plane-1.sh
@@ -7,5 +7,8 @@ echo "you can not install k8s using:"
 
 kubeadm init --control-plane-endpoint=${MACHINE_IP} --pod-network-cidr=10.1.0.0/16
 
+mkdir -p ${HOME}/.kube
+cp /etc/kubernetes/admin.conf ${HOME}/.kube/config
+
 wget  https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/canal.yaml
 kubectl apply -f canal.yaml
