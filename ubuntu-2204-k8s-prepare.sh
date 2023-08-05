2d02ba0d80d2d9b7b1cdcd66ddbb5c007f58dc4c
Sagi Forbes <sagi.forbes@myhome.loc>
2023-08-05 15:20:34 +0300

    dev

--- ubuntu-2204-k8s-prepare.sh
+++ ubuntu-2204-k8s-prepare.sh
@@ -11,6 +11,9 @@ overlay
 br_netfilter
 EOF
 
+modprobe overlay
+modprobe br_netfilter
+
 tee /etc/sysctl.d/kubernetes.conf <<EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
