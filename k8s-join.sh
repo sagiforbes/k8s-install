5c9cbd0f00f4c20493511fbad7b3cc5705feb864
Sagi Forbes <sagi.forbes@myhome.loc>
2023-08-05 16:47:52 +0300

    dev

--- k8s-join.sh
+++ k8s-join.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-echo "initial k8s"
-
 echo "on master control-plane node type:"
 echo "kubeadm token create --print-join-command"
 
+
+
 echo "to simply join use:"
 echo "kubeadm join [contron-plane-master-ip:6443] --token [from above command] --discovery-token-ca-cert-hash [from above command]"
+
+echo ""
+echo "after join install the containerd cni plugin"
+echo "wget https://raw.githubusercontent.com/containerd/containerd/main/script/setup/install-cni"
+echo "bash install-cni v1.3.0"
 
