82c9ffdd7b1bc9e8200a6ee52c69d45dcfe0415f
Sagi Forbes <sagi.forbes@myhome.loc>
2023-08-05 13:42:51 +0300

    dev

--- ubuntu-2204-k8s-reset.sh
+++ ubuntu-2204-k8s-reset.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 kubeadm reset -f --v=5
+echo "delete cni"
 rm -rf /etc/cni/net.d
+echo "delete iptables"
 iptables -F && iptables -t nat -F  && iptables -t mangle -F &&  iptables -X
 
