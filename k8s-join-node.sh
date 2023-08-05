85c73b5dd7360abff9ec4bf187d70b5ab19d8670
Sagi Forbes <sagi.forbes@myhome.loc>
2023-08-05 14:43:23 +0300

    dev

--- k8s-join-node.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "on master control-plane node type:"
-echo "kubeadm token create --print-join-command"
-
-
---
+++ k8s-join.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "on master control-plane node type:"
+echo "kubeadm token create --print-join-command"
+
+echo "to simply join use:
+echo "kubeadm join <contron-plane-master-ip>:6443 --token <from above command> --discovery-token-ca-cert-hash <from above command>"
+
