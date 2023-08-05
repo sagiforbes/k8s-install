3ecb830b4752db975a57caa441858c9d7fcf5113
Sagi Forbes <sagi.forbes@myhome.loc>
2023-08-05 22:06:51 +0300

    dev

--- 02-ubuntu-2204-k8s-install-control-plane-master.sh
+++ 02-ubuntu-2204-k8s-install-control-plane-master.sh
@@ -15,13 +15,26 @@ kubeadm init --control-plane-endpoint=${MACHINE_IP} --pod-network-cidr=$PODS_CIR
 mkdir -p ${HOME}/.kube
 cp /etc/kubernetes/admin.conf ${HOME}/.kube/config
 
+
+echo "remove taint from control plane, so pods can run on controling plane nodes"
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
+
 wget https://raw.githubusercontent.com/containerd/containerd/main/script/setup/install-cni
 bash install-cni v1.3.0
 
+
+echo "install network service"
 wget https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
 kubectl create -f tigera-operator.yaml
 
 wget https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/custom-resources.yaml
 
-yq
+
+echo "install metric service. Note the default command line changed to allow insecure node communication"
+echo "metric-server.yaml is based on https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml"
+kubectl apply -f metric-server.yaml
+
+
 
